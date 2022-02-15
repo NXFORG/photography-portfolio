941fd05805f11decfdff82e893c96ec68acb6f52
“nxforg” <christopher.anstead@yahoo.co.uk>
2022-02-15 12:22:23 +0000

    store photos in azure blob

--- src/Pages/Photography/index.jsx
+++ src/Pages/Photography/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './style.css';
 import { Grid } from '../../Components/index';
-import { first, second, third, seventh, eighth, ninth } from '../../Pictures/index';
 
 export const Photography = () => {
-    const photographyPictures = [first, second, third, seventh, eighth, ninth]
+
+    const photographyPictures = [
+        {'id': 1, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_155353.jpg'},
+        {'id': 2, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_183344.jpg'},
+        {'id': 3, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_155605.jpg'},
+        {'id': 4, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_155942.jpg'},
+        {'id': 5, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_155822.jpg'},
+        {'id': 6, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20211114_155928.jpg'},
+        {'id': 7, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220201_094031.jpg'},
+        {'id': 8, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113223.jpg'},
+        {'id': 9, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113353.jpg'},
+        {'id': 10, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113428.jpg'},
+        {'id': 11, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113522.jpg'},
+        {'id': 12, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113608.jpg'},
+        {'id': 13, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113719.jpg'},
+        {'id': 14, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_113749.jpg'},
+        {'id': 15, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_114449.jpg'},
+        {'id': 16, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_114531.jpg'},
+        {'id': 17, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_114827.jpg'},
+        {'id': 18, 'src':'https://bportfoliopictures.blob.core.windows.net/blackandwhite/IMG_20220213_114856.jpg'},
+        {'id': 19, 'src':'https://bportfoliopictures.blob.core.windows.net/colour/IMG_20211114_144927.jpg'},
+        {'id': 20, 'src':'https://bportfoliopictures.blob.core.windows.net/colour/IMG_20220213_114128.jpg'},
+        {'id': 21, 'src':'https://bportfoliopictures.blob.core.windows.net/colour/IMG_20220213_114206.jpg'},
+        {'id': 22, 'src':'https://bportfoliopictures.blob.core.windows.net/colour/IMG_20220213_114310.jpg'},
+        {'id': 23, 'src':'https://bportfoliopictures.blob.core.windows.net/colour/IMG_20220213_115914.jpg'},
+    ]
+
     return (
         <>
             <Grid pictures={photographyPictures}/>
---
+++ src/Pictures/B&W/index.js
@@ -0,0 +1,41 @@
+/* const images = [
+    { id: 1, src: './IMG_20211114_155353.jpg', title: 'Waterloo', description: 'London Waterloo Station Entrance/Exit' },
+    { id: 2, src: './IMG_20211114_155605.jpg', title: 'London Eye', description: 'London Eye' },
+    { id: 3, src: './IMG_20211114_155822.jpg', title: 'Apartments', description: 'Apartments' },
+    { id: 4, src: './IMG_20211114_155928.jpg', title: 'Apartments', description: 'Apartments' },
+    { id: 5, src: './IMG_20211114_155942.jpg', title: 'Waterloo', description: 'Waterloo' },
+    { id: 6, src: './IMG_20211114_183344.jpg', title: '', description: '' },
+    { id: 7, src: './IMG_20220201_094031.jpg', title: '', description: '' },
+    { id: 8, src: './IMG_20220213_113223.jpg', title: '', description: '' },
+    { id: 9, src: './IMG_20220213_113353.jpg', title: '', description: '' },
+    { id: 10, src: './IMG_20220213_113428.jpg', title: '', description: '' },
+    { id: 11, src: './IMG_20220213_113522.jpg', title: '', description: '' },
+    { id: 12, src: './IMG_20220213_113608.jpg', title: '', description: '' },
+    { id: 13, src: './IMG_20220213_113719.jpg', title: '', description: '' },
+    { id: 14, src: './IMG_20220213_113749.jpg', title: '', description: '' },
+    { id: 15, src: './IMG_20220213_114449.jpg', title: '', description: '' },
+    { id: 16, src: './IMG_20220213_114531.jpg', title: '', description: '' },
+    { id: 17, src: './IMG_20220213_114827.jpg', title: '', description: '' },
+    { id: 18, src: './IMG_20220213_114856.jpg', title: '', description: '' },
+];
+  
+export default images; */
+
+export { default as first } from './IMG_20211114_155353.jpg';
+export { default as second } from './IMG_20211114_155605.jpg';
+export { default as third } from './IMG_20211114_155822.jpg';
+export { default as fourth } from './IMG_20211114_155928.jpg';
+export { default as fifth } from './IMG_20211114_155942.jpg';
+export { default as sixth } from './IMG_20211114_183344.jpg';
+export { default as seventh } from './IMG_20220201_094031.jpg';
+export { default as eighth } from './IMG_20220213_113223.jpg';
+export { default as ninth } from './IMG_20220213_113353.jpg';
+export { default as tenth } from './IMG_20220213_113428.jpg';
+export { default as eleventh } from './IMG_20220213_113522.jpg';
+export { default as twelfth } from './IMG_20220213_113608.jpg';
+export { default as thirteenth } from './IMG_20220213_113719.jpg';
+export { default as fourteenth} from './IMG_20220213_113749.jpg';
+export { default as fifteenth } from './IMG_20220213_114449.jpg';
+export { default as sixteenth } from './IMG_20220213_114531.jpg';
+export { default as seventeenth } from './IMG_20220213_114827.jpg';
+export { default as eighteenth } from './IMG_20220213_114856.jpg';
